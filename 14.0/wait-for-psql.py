f282e896e10ab19c09984cb0abeaef80ae8b309c
lgh <lghbeta@gmail.com>
2021-10-01 19:00:09 +0800

    Updated to version 14.0.20211001

--- 14.0/wait-for-psql.py
+++ 14.0/wait-for-psql.py
@@ -3,9 +3,24 @@ import argparse
 import psycopg2
 import sys
 import time
+import os
 
 
 if __name__ == '__main__':
+    # Create default config
+    conf = '/etc/odoo/odoo.conf'
+    if not os.path.exists(conf):
+        f = open(conf, 'w')
+        f.write('[options]\naddons_path = /mnt/extra-addons\ndata_dir = /var/lib/odoo\n\n')
+        f.close()
+
+    # Check odoo packages
+    pkg = '/usr/lib/python3/dist-packages/odoo'
+    if not os.path.exists(pkg) or not os.listdir(pkg) or not os.path.exists(pkg + '/http.py'):
+        print('Odoo packages not found, Mount volume %s and put the odoo source code into it' % pkg,
+              file=sys.stderr)
+        sys.exit(1)
+
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--db_host', required=True)
     arg_parser.add_argument('--db_port', required=True)
--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # About this Repo
 
 This is the Git repo of the Docker image for Odoo environment. 
+
+`docker-compose.yml` Example:
+
+```yaml
+version: '2'
+services:
+  odoo-env:
+    container_name: odoo-env
+    image: aniven/odoo-env:14
+    restart: unless-stopped
+    ports:
+      - 8069:8069
+      - 8071:8071
+      - 8072:8072
+    environment:
+      - HOST=172.19.0.10
+      - USER=odoo
+      - PASSWORD=odoo
+    volumes:
+      - ./conf:/etc/odoo
+      - ./packages:/usr/lib/python3/dist-packages/odoo
+      - ./web-data:/var/lib/odoo
+      - ./extra-addons:/mnt/extra-addons
+    network_mode: bridge
+```
+
+> You must set the owner ID to 101 for the mounted volume, e.g.: `chown 101:101 ./web-data`
+
